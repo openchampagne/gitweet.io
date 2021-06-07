efef64fba20092a4cbc926ced1ad059e992cd720
Clément Rigo <rigoclement@mydnic.be>
2021-06-07 08:42:48 +0200

    move to job and skip if dependabot
    Gitweet.io will now skip commits from dependabot

--- app/Http/Controllers/Api/GithubWebHookController.php
+++ app/Http/Controllers/Api/GithubWebHookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Pipeline;
-use Abraham\TwitterOAuth\TwitterOAuth;
+use App\Jobs\TweetNewCommit;
 
 class GithubWebHookController extends Controller
 {
@@ -15,33 +15,6 @@ class GithubWebHookController extends Controller
 
     public function store(Pipeline $pipeline)
     {
-        $message = $this->getMessage(request()->input('head_commit.message'));
-
-        if (!empty($message) && $pipeline->twitter_access_code) {
-            $pipeline->increment('count');
-
-            $this->tweet($message, $pipeline);
-        }
-    }
-
-    private function getMessage(string $commitMessage)
-    {
-        if (($pos = strpos($commitMessage, "\n\n")) !== false) {
-            return trim(substr($commitMessage, $pos + 1));
-        }
-
-        return null;
-    }
-
-    private function tweet(string $message, Pipeline $pipeline)
-    {
-        $connection = new TwitterOAuth(
-            env('TWITTER_CLIENT_ID'),
-            env('TWITTER_CLIENT_SECRET'),
-            $pipeline->twitter_access_code,
-            $pipeline->twitter_access_code_secret
-        );
-
-        ld($connection->post('statuses/update', ['status' => $message]));
+        dispatch(new TweetNewCommit($pipeline, request()->all()));
     }
 }
---
+++ app/Jobs/TweetNewCommit.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Pipeline;
+use Illuminate\Support\Str;
+use Illuminate\Bus\Queueable;
+use Abraham\TwitterOAuth\TwitterOAuth;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class TweetNewCommit implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $pipeline;
+    public $message;
+    public $pusher;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Pipeline $pipeline, array $githubPayload)
+    {
+        $this->pipeline = $pipeline;
+        $this->message = $this->getMessage($githubPayload['head_commit']['message']);
+        $this->pusher = $githubPayload['pusher']['name'];
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if ($this->shouldPostTweet()) {
+            $this->pipeline->increment('count');
+
+            $this->tweet($this->message, $this->pipeline);
+        }
+    }
+
+    private function getMessage(string $commitMessage)
+    {
+        if (($pos = strpos($commitMessage, "\n\n")) !== false) {
+            return trim(substr($commitMessage, $pos + 1));
+        }
+
+        return null;
+    }
+
+    private function shouldPostTweet(): bool
+    {
+        return !empty($this->message) && $this->pipeline->twitter_access_code && !$this->isPusherABot();
+    }
+
+    private function isPusherABot(): bool
+    {
+        return Str::containsAll([
+            '[bot]',
+            // ...
+        ], $this->pusher);
+    }
+
+    private function tweet(string $message, Pipeline $pipeline)
+    {
+        $connection = new TwitterOAuth(
+            config('services.twitter.client_id'),
+            config('services.twitter.client_secret'),
+            $pipeline->twitter_access_code,
+            $pipeline->twitter_access_code_secret
+        );
+
+        $connection->post('statuses/update', ['status' => $message]);
+    }
+}
